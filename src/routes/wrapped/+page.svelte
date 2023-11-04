4f0e5f1edf619a88967f7084a61b557abbfa3a30
comfortablecoffee <csdropout64@gmail.com>
2023-11-04 17:13:13 -0400

    m

--- src/routes/wrapped/+page.svelte
+++ src/routes/wrapped/+page.svelte
@@ -2,10 +2,11 @@
     import Level from "$lib/wrapper/level/level.svelte";
     import WrapW from "$lib/wrapper/wrap_m/wrap_w.svelte";
     import Learn from "$lib/wrapper/learn_time/learn.svelte";
+    import SkillsSum from "$lib/wrapper/skills_sum/skills_sum.svelte";
     
     import Upload from "$lib/wrapper/upload.svelte";
 	import { onDestroy, onMount } from "svelte";
-    let v = 1;
+    let v = 4;
 
     function updateV() {
         if (v === 1) {
@@ -13,6 +14,8 @@
         } else if (v === 2) {
             v = 3;
         } else if (v === 3) {
+            v = 4;
+        } else if (v === 4) {
             v = 1;
         }
     }
@@ -39,11 +42,17 @@
     {#if v ===3 }
         <Learn />
     {/if}
+    {#if v ===4 }
+        <SkillsSum />
+    {/if}
 </div>
 
 <div class="absolute bottom-6 w-full flex justify-center items-center">
     <button class="w-5 h-5"><Upload /></button>
     <button class="pl-2">Share Your Journey</button>
 </div>
+
+
+
 
 
