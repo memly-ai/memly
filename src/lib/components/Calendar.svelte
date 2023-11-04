31f9325c243bbfe260b11324c752d28bc704e141
Albert Terc <albytterc@protonmail.com>
2023-11-04 17:36:04 -0400

    Add missing calendar pages from prev commit

---
+++ src/lib/components/Calendar.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	export let currentDate: Date;
+	export let data;
+
+	const monthNames = [
+		'January',
+		'February',
+		'March',
+		'April',
+		'May',
+		'June',
+		'July',
+		'August',
+		'September',
+		'October',
+		'November',
+		'December'
+	];
+</script>
+
+<div class="mb-10 rounded-xl bg-white shadow-lg">
+	<div class="rounded-t-xl bg-[#DC5E65]">
+		<h1 class="font-inter p-2 text-center text-2xl font-bold text-white">
+			{monthNames[currentDate.getMonth() - 1]}
+		</h1>
+	</div>
+	<div class="grid grid-cols-7 gap-4 pb-5 pl-5">
+		<p class="opacity-25">S</p>
+		<p class="opacity-25">M</p>
+		<p class="opacity-25">T</p>
+		<p class="opacity-25">W</p>
+		<p class="opacity-25">Th</p>
+		<p class="opacity-25">F</p>
+		<p class="opacity-25">S</p>
+		<div></div>
+		<div></div>
+		<div></div>
+		{#each { length: 30 } as _, i}
+			{#if i + 1 === currentDate.getDate()}
+				<div class="font-bold">{i + 1}</div>
+			{:else}
+				<div>{i + 1}</div>
+			{/if}
+		{/each}
+	</div>
+</div>
