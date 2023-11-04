212bc07cf8fede785f991ab2905f1f9e2a4ab1b2
andrewtlu <andrewlulu2012@gmail.com>
2023-11-04 14:01:36 -0400

    create progress nav mockup

---
+++ src/routes/progress/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    import Cog from '$lib/cog.svelte'
+    // script to generate path
+    // script to load popup?
+</script>
+
+<nav class="fixed top-0 right-0 left-0 flex items-center justify-between p-2 bg-white shadow-md">
+    <!-- logo -->
+    <img class="w-16" src="/logo.png" alt="memly logo">
+    <!-- progress -->
+    <div class="w-full p-4">
+        <div class="w-full bg-gray-200 rounded-full h-6 dark:bg-gray-700">
+            <div class="bg-blue-600 h-6 rounded-full" style="width: 45%"></div>
+        </div>
+    </div>
+    <!-- settings -->
+    <div class="p-2">
+        <Cog />
+    </div>
+</nav>
+
+<div class="">
+    <!-- section info -->
+</div>
+
+<!-- for each subsection generate path -->
+<div>
+    <!-- entrepreneurship section x -->
+</div>
+
+<div>
+    <!-- locked section 2 -->
+</div>
+
