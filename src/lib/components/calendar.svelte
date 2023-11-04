0e47fa518f35443075f44a83e1ddef9fbb967afa
Albert Terc <albytterc@protonmail.com>
2023-11-04 17:36:43 -0400

    add calendar again

--- src/lib/components/calendar.svelte
+++ src/lib/components/calendar.svelte
@@ -1,7 +1,46 @@
-<div class="w-5">
-	<svg viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg"
-		><path
-			d="M160 32V64H288V32C288 14.33 302.3 0 320 0C337.7 0 352 14.33 352 32V64H400C426.5 64 448 85.49 448 112V160H0V112C0 85.49 21.49 64 48 64H96V32C96 14.33 110.3 0 128 0C145.7 0 160 14.33 160 32zM0 192H448V464C448 490.5 426.5 512 400 512H48C21.49 512 0 490.5 0 464V192zM64 304C64 312.8 71.16 320 80 320H112C120.8 320 128 312.8 128 304V272C128 263.2 120.8 256 112 256H80C71.16 256 64 263.2 64 272V304zM192 304C192 312.8 199.2 320 208 320H240C248.8 320 256 312.8 256 304V272C256 263.2 248.8 256 240 256H208C199.2 256 192 263.2 192 272V304zM336 256C327.2 256 320 263.2 320 272V304C320 312.8 327.2 320 336 320H368C376.8 320 384 312.8 384 304V272C384 263.2 376.8 256 368 256H336zM64 432C64 440.8 71.16 448 80 448H112C120.8 448 128 440.8 128 432V400C128 391.2 120.8 384 112 384H80C71.16 384 64 391.2 64 400V432zM208 384C199.2 384 192 391.2 192 400V432C192 440.8 199.2 448 208 448H240C248.8 448 256 440.8 256 432V400C256 391.2 248.8 384 240 384H208zM320 432C320 440.8 327.2 448 336 448H368C376.8 448 384 440.8 384 432V400C384 391.2 376.8 384 368 384H336C327.2 384 320 391.2 320 400V432z"
-		/></svg
-	>
+<script lang="ts">
+	export let currentDate: Date;
+	export let data;
+
+	const monthNames = [
+		'January',
+		'February',
+		'March',
+		'April',
+		'May',
+		'June',
+		'July',
+		'August',
+		'September',
+		'October',
+		'November',
+		'December'
+	];
+</script>
+
+<div class="mb-10 rounded-xl bg-white shadow-lg">
+	<div class="rounded-t-xl bg-[#DC5E65]">
+		<h1 class="font-inter p-2 text-center text-2xl font-bold text-white">
+			{monthNames[currentDate.getMonth() - 1]}
+		</h1>
+	</div>
+	<div class="grid grid-cols-7 gap-4 pb-5 pl-5">
+		<p class="opacity-25">S</p>
+		<p class="opacity-25">M</p>
+		<p class="opacity-25">T</p>
+		<p class="opacity-25">W</p>
+		<p class="opacity-25">Th</p>
+		<p class="opacity-25">F</p>
+		<p class="opacity-25">S</p>
+		<div></div>
+		<div></div>
+		<div></div>
+		{#each { length: 30 } as _, i}
+			{#if i + 1 === currentDate.getDate()}
+				<div class="font-bold">{i + 1}</div>
+			{:else}
+				<div>{i + 1}</div>
+			{/if}
+		{/each}
+	</div>
 </div>
