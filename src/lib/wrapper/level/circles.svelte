bcdaab254267f9b717b776f4a3ec7f0280ca24d5
andrewtlu <andrewlulu2012@gmail.com>
2023-11-04 19:36:07 -0400

    initial

--- src/lib/wrapper/level/circles.svelte
+++ src/lib/wrapper/level/circles.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
     export let x = 0;
 </script>
+
 <div class="relative w-15 h-15">
     <svg class="w-full h-full" viewBox="0 0 100 100">
       <!-- Background circle -->
       <circle
         class="text-gray-200 stroke-current"
         stroke-width="10"
-        
         cx="50"
         cy="50"
         r="40"
@@ -15,18 +15,18 @@
       ></circle>
       <!-- Progress circle -->
       <circle
-        class="text-indigo-500  progress-ring__circle stroke-current"
+        class="text-indigo-500 progress-ring__circle stroke-current"
         stroke-width="10"
         stroke-linecap="round"
         cx="50"
         cy="50"
         r="40"
         fill="transparent"
-        stroke-dashoffset="calc(400 - (400 * 45) / 100)"
+        stroke-dashoffset="10"
       ></circle>
       
       <!-- Center text -->
-      <text x="50" y="50" font-family="Verdana" font-size="12" text-anchor="middle" alignment-baseline="middle">{x}%</text>
+      <text x="50" y="50" font-family="Inter" font-size="12" text-anchor="middle" alignment-baseline="middle">{x}%</text>
   
     </svg>
   </div>
--- src/lib/wrapper/level/level.svelte
+++ src/lib/wrapper/level/level.svelte
@@ -2,20 +2,22 @@
     import Circles from "./circles.svelte";
 </script>
 
-<div class="pl-5">
-    <h1 class="text-5xl" style="font-size: 30px; padding-top: 240px; padding-bottom: 40px;">Of the 53 skills...</h1>
-    <div class="flex flex-row justify-flex-start">
-        <div class="flex flex-col items-center w-full">
-            <Circles x = {70}/>
-            <h1 class="text-5xl text-base p-1.5">Beginner</h1>
-        </div>
-        <div class="flex flex-col items-center w-full">
-            <Circles x = {70}/>
-            <h1 class="text-5xl text-base p-1.5">Intermediate</h1>
-        </div>
-        <div class="flex flex-col items-center w-full">
-            <Circles x = {70}/>
-            <h1 class="text-5xl text-base p-1.5">Advance</h1>
+<div class="px-5 flex items-middle">
+    <div class="p-5 shadow-xl rounded-md w-full">
+        <h1 class="text-5xl" style="font-size: 30px; padding-top: 240px; padding-bottom: 40px;">Of the 53 skills...</h1>
+        <div class="flex flex-row justify-flex-start">
+            <div class="flex flex-col items-center w-full">
+                <Circles x = {70}/>
+                <h1 class="text-5xl text-base p-1.5">Beginner</h1>
+            </div>
+            <div class="flex flex-col items-center w-full">
+                <Circles x = {70}/>
+                <h1 class="text-5xl text-base p-1.5">Intermediate</h1>
+            </div>
+            <div class="flex flex-col items-center w-full">
+                <Circles x = {70}/>
+                <h1 class="text-5xl text-base p-1.5">Advance</h1>
+            </div>
         </div>
     </div>
 </div>
